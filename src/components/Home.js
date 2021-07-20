b0cb7945d0f1325b9488b822893809570a1d3b7a
Bryan Leyva <bryan.leyva@spaceforce.mil>
2021-07-21 00:02:37 +0200

    nav bar

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+
+function Home(){
+
+    return(
+        <body>
+
+        </body>
+    )
+}
+
+export default Home
